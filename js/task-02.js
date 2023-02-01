5fddc433a92d72baec1e708a089fb4ba1955645a
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-02-01 22:37:02 +0200

    finished task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const elements = [];
+ingredients.forEach(element => {
+  const oneIngridient = document.createElement('li');
+  oneIngridient.classList.add('item');
+  oneIngridient.textContent = element;
+  elements.push(oneIngridient);
+});
+
+const listOfIngridients = document.querySelector('#ingredients');
+listOfIngridients.append(...elements);
