02abcbf0a389d30b809d74280a86f098358c4992
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-02-03 22:31:41 +0200

    finished task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
+const form = document.querySelector('.login-form');
+const newUser = {};
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  // збір даних з допомогою FormData
+  // const formData = new FormData(event.currentTarget);
+  // formData.forEach((value, name) => {
+  //   console.log('name:', name);
+  //   console.log('value:', value);
+  // });
+
+  // збір даних з допомогою elements
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email === '' || password === '') {
+    alert('Please fill in all fields of the form');
+  } else {
+    newUser.email = email;
+    newUser.password = password;
+    console.log(newUser);
+  }
+
+  event.currentTarget.reset();
+}
