f7feb55ce8651399ebc28f3c420162f546d584b7
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-02-04 23:10:35 +0200

    finished task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const changeColorBtn = document.querySelector('.change-color');
+const changedColorInput = document.querySelector('.color');
+const body = document.body;
+
+changeColorBtn.addEventListener('click', changeColor);
+
+function changeColor(event) {
+  const changedColor = getRandomHexColor();
+  changedColorInput.textContent = changedColor;
+  body.style.backgroundColor = changedColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
