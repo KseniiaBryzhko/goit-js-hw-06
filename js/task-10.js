c8dc1a8bed5ba3daf44787d9fd408bc3a83bd9f6
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-02-05 00:30:41 +0200

    finished task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
+const input = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const listOfBoxes = document.querySelector('#boxes');
+
+input.addEventListener('input', changedValue);
+createBtn.addEventListener('click', () => {
+  createBoxes(Number(input.value));
+});
+destroyBtn.addEventListener('click', destroyBoxes);
+
+let inputValue = 0;
+
+function changedValue(event) {
+  inputValue = event.currentTarget.value;
+}
+
+function createBoxes(amount) {
+  let arrayOfBoxes = [];
+  let sizes = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const oneBox = document.createElement('div');
+    sizes += 10;
+    oneBox.style.height = `${sizes}px`;
+    oneBox.style.width = `${sizes}px`;
+    oneBox.style.backgroundColor = getRandomHexColor();
+    arrayOfBoxes.push(oneBox);
+  }
+  return listOfBoxes.append(...arrayOfBoxes);
+}
+
+function destroyBoxes() {
+  return (listOfBoxes.innerHTML = '');
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
