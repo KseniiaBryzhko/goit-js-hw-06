32c0d0c4f368eccc4b8303fde1e6770dfb3916e1
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-02-06 23:55:33 +0200

    added edits from mentor task-08

--- js/task-08.js
+++ js/task-08.js
@@ -6,25 +6,49 @@ form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
   event.preventDefault();
 
-  // збір даних з допомогою FormData
-  // const formData = new FormData(event.currentTarget);
-  // formData.forEach((value, name) => {
-  //   console.log('name:', name);
-  //   console.log('value:', value);
-  // });
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
 
-  // збір даних з допомогою elements
-  const formElements = event.currentTarget.elements;
-  const email = formElements.email.value;
-  const password = formElements.password.value;
-
-  if (email === '' || password === '') {
+  if (email.value === '' || password.value === '') {
     alert('Please fill in all fields of the form');
   } else {
-    newUser.email = email;
-    newUser.password = password;
+    newUser.email = email.value;
+    newUser.password = password.value;
     console.log(newUser);
   }
 
   event.currentTarget.reset();
 }
+
+// попередній варіант
+// const form = document.querySelector('.login-form');
+// const newUser = {};
+
+// form.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+// збір даних з допомогою FormData
+// const formData = new FormData(event.currentTarget);
+// formData.forEach((value, name) => {
+//   console.log('name:', name);
+//   console.log('value:', value);
+// });
+
+// збір даних з допомогою elements
+//   const formElements = event.currentTarget.elements;
+//   const email = formElements.email.value;
+//   const password = formElements.password.value;
+
+//   if (email === '' || password === '') {
+//     alert('Please fill in all fields of the form');
+//   } else {
+//     newUser.email = email;
+//     newUser.password = password;
+//     console.log(newUser);
+//   }
+
+//   event.currentTarget.reset();
+// }
