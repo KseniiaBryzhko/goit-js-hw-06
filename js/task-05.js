5f45a6bfea1e62db7b7fd6f80775afc93eb2b518
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-02-06 23:26:36 +0200

    added edits from mentor task-05

--- js/task-05.js
+++ js/task-05.js
@@ -4,5 +4,17 @@ const output = document.querySelector('#name-output');
 input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-  output.textContent = event.currentTarget.value;
+  output.textContent = event.currentTarget.value.trim()
+    ? event.currentTarget.value.trim()
+    : 'Anonymous';
 }
+
+// попередній варіант
+// const input = document.querySelector('#name-input');
+// const output = document.querySelector('#name-output');
+
+// input.addEventListener('input', onInputChange);
+
+// function onInputChange(event) {
+//   output.textContent = event.currentTarget.value;
+// }
