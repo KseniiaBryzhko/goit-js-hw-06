dd67aa9df1149772a710594cdf828d5e5909b37f
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-02-06 23:11:19 +0200

    added edits from mentor task-03

--- js/task-03.js
+++ js/task-03.js
@@ -13,6 +13,32 @@ const images = [
   },
 ];
 
+const imagesMarkUp = images
+  .map(
+    image =>
+      `<li class = "third-task-item"><img class = "third-task-image" src = "${image.url}" alt = "${image.alt}" /></li>`
+  )
+  .join('');
+
+const listOfImages = document.querySelector('.gallery');
+listOfImages.insertAdjacentHTML('afterbegin', imagesMarkUp);
+
+// попередній варіант
+// const images = [
+//   {
+//     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'White and Black Long Fur Cat',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+//   },
+//   {
+//     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Group of Horses Running',
+//   },
+// ];
+
 // додавання через append
 // const makeImage = ({ url, alt }) => {
 //   const oneItem = document.createElement('li');
@@ -28,10 +54,10 @@ const images = [
 // listOfImages.append(...createdImages);
 
 // додавання через insertAdjacentHTML
-const listOfImages = document.querySelector('.gallery');
-images.forEach(image => {
-  listOfImages.insertAdjacentHTML(
-    'afterbegin',
-    `<li class = "third-task-item"><img class = "third-task-image" src = "${image.url}" alt = "${image.alt}" /></li>`
-  );
-});
+// const listOfImages = document.querySelector('.gallery');
+// images.forEach(image => {
+//   listOfImages.insertAdjacentHTML(
+//     'afterbegin',
+//     `<li class = "third-task-item"><img class = "third-task-image" src = "${image.url}" alt = "${image.alt}" /></li>`
+//   );
+// });
