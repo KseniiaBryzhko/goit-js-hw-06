ccb72a0794f60b8bc0c6a33f7f2d2743a85d11f9
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-02-06 22:47:15 +0200

    added edits from mentor task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,12 +1,25 @@
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 
-const elements = [];
-ingredients.forEach(element => {
+const elements = ingredients.map(element => {
   const oneIngridient = document.createElement('li');
   oneIngridient.classList.add('item');
   oneIngridient.textContent = element;
-  elements.push(oneIngridient);
+  return oneIngridient;
 });
 
 const listOfIngridients = document.querySelector('#ingredients');
 listOfIngridients.append(...elements);
+
+// попередній варіант
+// const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+// const elements = [];
+// ingredients.forEach(element => {
+//   const oneIngridient = document.createElement('li');
+//   oneIngridient.classList.add('item');
+//   oneIngridient.textContent = element;
+//   elements.push(oneIngridient);
+// });
+
+// const listOfIngridients = document.querySelector('#ingredients');
+// listOfIngridients.append(...elements);
