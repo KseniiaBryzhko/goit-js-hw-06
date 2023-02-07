ed345c111fc3451cfcd0c00ae723fb017b5ebc03
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-02-07 16:47:16 +0200

    added edits from mentor task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,21 @@
 const input = document.querySelector('#font-size-control');
 const output = document.querySelector('#text');
 
+output.style.fontSize = `${input.value}px`;
+
 input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-  const changedFontSize = input.value;
-  output.style.fontSize = `${changedFontSize}px`;
+  output.style.fontSize = `${input.value}px`;
 }
+
+// попередній варіант
+// const input = document.querySelector('#font-size-control');
+// const output = document.querySelector('#text');
+
+// input.addEventListener('input', onInputChange);
+
+// function onInputChange(event) {
+//   const changedFontSize = input.value;
+//   output.style.fontSize = `${changedFontSize}px`;
+// }
